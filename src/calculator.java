290bb232f34ab7a01aedc4ad73b685d86961c91a
ArsalanKhalid08 <Administrator@ELAB708-40.iqra.edu.pk>
2024-11-28 16:55:58 +0500

    Added modular methods for message display and sum calculation

---
+++ src/calculator.java
@@ -0,0 +1,21 @@
+public class calculator {
+
+    /**
+     *
+     * @param a
+     * @param b
+     * @return result
+     */
+    public int calculateSum(int a, int b){
+        int result=a+b;
+        return result;
+    }
+
+    /**
+     * @return message
+     */
+    public String printMessage(){
+        String message ="Welcome to Software Construction";
+        return message;
+    }
+}
